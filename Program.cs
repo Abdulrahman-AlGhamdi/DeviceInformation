9e90c995c40837c78ce47cc2a8b5e94da5c0bc27
Abdulrahman-AlGhamdi <XxD7ooom.SSxX@Gmail.Com>
2021-12-24 22:25:03 +0300

    Get all CPU information needed

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 
 ManagementObjectSearcher os = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
 
+System.Console.WriteLine("OS Information\n");
+
 foreach (ManagementObject info in os.Get()) {
     PropertyData osName = info.Properties["Caption"];
     Console.WriteLine("OS Name  	  : {0}", osName.Value);
@@ -26,4 +28,39 @@ foreach (ManagementObject info in os.Get()) {
     
     PropertyData osVersion = info.Properties["Version"];
     Console.WriteLine("OS Version	  : {0}", osVersion.Value);
+}
+
+System.Console.WriteLine("\n------------------------------------------------------\n");
+
+ManagementObjectSearcher cpu = new ManagementObjectSearcher("select * from Win32_Processor");
+
+System.Console.WriteLine("CPU Information\n");
+
+foreach (ManagementObject info in cpu.Get()) {
+    PropertyData cpuName = info.Properties["Name"];
+    Console.WriteLine("CPU Name		: {0}", cpuName.Value);
+
+    PropertyData processorId = info.Properties["ProcessorId"];
+    Console.WriteLine("Processor Id		: {0}", processorId.Value);
+
+    PropertyData currentClockSpeed = info.Properties["CurrentClockSpeed"];
+    Console.WriteLine("Current Clock Speed	: {0}", currentClockSpeed.Value);
+
+    PropertyData numberOfCores = info.Properties["NumberOfCores"];
+    Console.WriteLine("Number Of Cores		: {0}", numberOfCores.Value);
+
+    PropertyData numberOfEnabledCore = info.Properties["NumberOfEnabledCore"];
+    Console.WriteLine("Number Of Enabled Cores : {0}", numberOfEnabledCore.Value);
+
+    PropertyData threadCount = info.Properties["ThreadCount"];
+    Console.WriteLine("Thread Count	        : {0}", threadCount.Value);
+    
+    PropertyData loadPercentage = info.Properties["LoadPercentage"];
+    Console.WriteLine("Load Percentage         : {0}", loadPercentage.Value);
+
+    PropertyData manufacturer = info.Properties["Manufacturer"];
+    Console.WriteLine("Manufacturer Name       : {0}", manufacturer.Value);
+
+    PropertyData socketDesignation = info.Properties["SocketDesignation"];
+    Console.WriteLine("Socket Designation      : {0}", socketDesignation.Value);
 }
